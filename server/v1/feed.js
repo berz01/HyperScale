79beba1d12961b2b1b445090f5c290bd4df2b466
ereio <Tte11@my.fsu.edu>
2017-06-14 11:48:08 -0400

    adding server git ignore

--- server/v1/feed.js
+++ server/v1/feed.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var request = require('request');
-var rp = require('request-promise');
+var requestpee = require('request-promise');
 
 //require('request-debug')(request);
 
@@ -13,13 +13,35 @@ api.use(bodyParser.urlencoded({
   extended: true
 }));
 
-var subKey = "0ea5ebb2c0e149e383f3f128574476ce"; 
+var subKey = "0ea5ebb2c0e149e383f3f128574476ce";
 
 api.post("/computerVision", function(req, res) {
   var myJSONObject = {
       url: req.body.url,
   };
 
+  // REQUEST PROMISE EXAMPLE AKA REQUEST PEE PEE
+  var options = {
+      uri: 'https://api.github.com/user/repos',
+      qs: {
+          access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
+      },
+      headers: {
+          'User-Agent': 'Request-Promise'
+      },
+      json: true // Automatically parses the JSON string in the response
+  };
+
+  requestpee(options)
+      .then(function (repos) {
+          console.log('User has %d repos', repos.length);
+      })
+      .catch(function (err) {
+          // API call failed...
+      });
+
+
+  // REGULAR REQUEST EXAMPLE
   request({
       url: "https://eastus2.api.cognitive.microsoft.com/vision/v1.0/describe?maxCandidates=1",
       method: "POST",
