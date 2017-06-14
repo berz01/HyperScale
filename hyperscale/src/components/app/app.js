d3463e2d171da4ed11bd92d0a35d56890bd5209b
ereio <Tte11@my.fsu.edu>
2017-06-14 15:57:29 -0400

    got nav and sidebar down, pages have been routed

---
+++ hyperscale/src/components/app/app.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './app.css';
+
+import Nav from '../nav/nav';
+import Sidebar from '../sidebar/sidebar';
+
+import Feed from '../feed/feed';
+import Opportunity from '../opportunity/opportunity';
+import Retention from '../retention/retention';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+export default class App extends Component {
+  constructor() {
+      super();
+  };
+
+  render() {
+    return (
+    <Router>
+      <div className="App">
+        <div className="page-container-bg-solid page-header-fixed page-sidebar-closed-hide-logo">
+          <Nav />
+          <div className="clearfix"> </div>
+          <div className="page-container">
+            <Sidebar />
+            <div className="page-content-wrapper">
+              <Route path = "feed" component = {Feed} />
+              <Route path = "retention" component = {Retention} />
+              <Route path = "opportunity" component = {Opportunity} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
+    );
+  };
+};
---
+++ hyperscale/src/components/retention/retention.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './retention.css';
+
+export default class Retention extends Component {
+  render() {
+    return (
+      <h1> Retention </h1>
+    );
+  }
+}
