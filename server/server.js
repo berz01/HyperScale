f89cce37de37bb36fa1e0ac3d8e17f029ddfa703
ereio <Tte11@my.fsu.edu>
2017-06-14 11:55:44 -0400

    server template running

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // External frameworks and dependencies
 const request = require('request');
-const express = require('express'); 
+const express = require('express');
+const session = require('express-session');
 const Promise = require('bluebird');
 const https = require('https');
 
@@ -27,15 +28,15 @@ app.get('/api/v1/*', function(req, res, next) {
     next();
 });
 
-app.use('/api/v1/call', require('./server/v1/routes/makecall.js'));
-app.use('/api/v1/text', require('./server/v1/routes/maketext.js'));
-app.use('/api/v1/email', require('./server/v1/routes/makeemail.js'));
-app.use('/api/v1/retention', require('./server/v1/routes/retention.js'));
-app.use('/api/v1/leads', require('./server/v1/routes/leads.js'));
-app.use('/api/v1/feed', require('./server/v1/routes/feed.js'));
+app.use('/api/v1/call', require('./v1/routes/makecall.js'));
+app.use('/api/v1/text', require('./v1/routes/maketext.js'));
+app.use('/api/v1/email', require('./v1/routes/makeemail.js'));
+app.use('/api/v1/retention', require('./v1/routes/retention.js'));
+app.use('/api/v1/leads', require('./v1/routes/leads.js'));
+app.use('/api/v1/feed', require('./v1/routes/feed.js'));
 
 
 // Start server
 var salesIntelServer = app.listen(port, function() {
-    console.log("\n", salesIntelServer.address().port);
+    console.log("\nhyper server is now hosted on port: ", salesIntelServer.address().port);
 });
